739ea47fc1b17352e067cd81d4a59d8c3373ae5e
Brendan Magee <bmagee@gmail.com>
2016-02-22 12:52:28 -0500

    updated readme; added arrays of vocab words

---
+++ script.js
@@ -0,0 +1,8 @@
+// I expect to define the entire game as an object, much as we did with the hangman game or high-card-js game.
+
+
+var flashCardDrill = {
+  var latinWords = ["m&emacr; (pronoun)" , "quid (pronoun)" , "nihil (noun)" , "n&omacr;n (adverb)" , "saepe (adverb)" , "s&imacr; (conjunction)" , "amo, amare, amavi, amatum (verb)" , "cogito, cogitare, cogitavi, cogitatum (verb)" , "debeo, deb&emacr;re, debui, debitum (verb)" , "do, dare, dedi, datum (irreg. verb)", "erro, errare, erravi, erratum (verb)" , "laudo, laudare, laudavi, laudatum (verb)" , "moneo, mon&emacr;re, monui, monitum (verb)" , "salveo, salv&emacr;re, &mdash; (verb) &ndash; salv&emacr; (sing.), salv&emacr;te (pl.)" , "servo, servare, servavi, servatum (verb)" , "conservo, conservare, conservavi, conservatum (verb)" , "terreo, terr$emacr;re, terrui, territum (verb)" , "valeo, val&emacr;re, valui, valiturum (verb)" , "video, vid&emacr;re, vidi, visum (verb)" , "voco, vocare, vocavi, vocatus (verb)"],
+  var englishWords = ["me" , "what" , "nothing" , "not" , "often" , "if" , "to love" , "to think" , "to owe" , "to give" , "to wander" , "to praise" , "to remind" , "to be well" , "to preserve" , "to conserve" , "to frighten" , "to be strong" , "to see" , "to call"],
+  
+}
