3d02db6a9dd27fda0a6d910663a7edb91b6e3d1f
Brendan Magee <bmagee@gmail.com>
2016-02-23 14:10:58 -0500

    combined two arrays into a single nested array

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-// I expect to define the entire game as an object, much as we did with the hangman game or high-card-js game.
+/*Start with one array of vocabulary items.
+  -
 
+*/
+var flashCard = [];
 
-var flashCardDrill = {
-  var latinWords = ["m&emacr; (pronoun)" , "quid (pronoun)" , "nihil (noun)" , "n&omacr;n (adverb)" , "saepe (adverb)" , "s&imacr; (conjunction)" , "amo, amare, amavi, amatum (verb)" , "cogito, cogitare, cogitavi, cogitatum (verb)" , "debeo, deb&emacr;re, debui, debitum (verb)" , "do, dare, dedi, datum (irreg. verb)", "erro, errare, erravi, erratum (verb)" , "laudo, laudare, laudavi, laudatum (verb)" , "moneo, mon&emacr;re, monui, monitum (verb)" , "salveo, salv&emacr;re, &mdash; (verb) &ndash; salv&emacr; (sing.), salv&emacr;te (pl.)" , "servo, servare, servavi, servatum (verb)" , "conservo, conservare, conservavi, conservatum (verb)" , "terreo, terr$emacr;re, terrui, territum (verb)" , "valeo, val&emacr;re, valui, valiturum (verb)" , "video, vid&emacr;re, vidi, visum (verb)" , "voco, vocare, vocavi, vocatus (verb)"],
-  var englishWords = ["me" , "what" , "nothing" , "not" , "often" , "if" , "to love" , "to think" , "to owe" , "to give" , "to wander" , "to praise" , "to remind" , "to be well" , "to preserve" , "to conserve" , "to frighten" , "to be strong" , "to see" , "to call"],
-  
-}
+var vocabWords = [["m&emacr; (pronoun)" , "me"] , ["quid (pronoun)" , "what"] , ["nihil (noun)" , "nothing"] , ["n&omacr;n (adverb)" , "not"] , ["saepe (adverb)" , "often"] , ["s&imacr; (conjunction)" , "if"] , ["amo, amare, amavi, amatum (verb)" , "to love"] , ["cogito, cogitare, cogitavi, cogitatum (verb)" , "to think"] , ["debeo, deb&emacr;re, debui, debitum (verb)" , "to owe"] , ["do, dare, dedi, datum (irreg. verb)" , "to give"] , ["erro, errare, erravi, erratum (verb)" , "to wander"] , ["laudo, laudare, laudavi, laudatum (verb)" , "to praise"] , ["moneo, mon&emacr;re, monui, monitum (verb)" , "to remind"] , ["salveo, salv&emacr;re, &mdash; (verb) &ndash; salv&emacr; (sing.), salv&emacr;te (pl.)" , "to be well"] , ["servo, servare, servavi, servatum (verb)" , "to preserve"] , ["conservo, conservare, conservavi, conservatum (verb)" , "to conserve"] , ["terreo, terr&emacr;re, terrui, territum (verb)" , "to frighten"] , ["valeo, val&emacr;re, valui, valiturum (verb)" , "to be strong"] , ["video, vid&emacr;re, vidi, visum (verb)" , "to see"] , ["voco, vocare, vocavi, vocatus (verb)" , "to call"]];
+
+var displayLatin = function() {
+    document.querySelector(".Latin").innerHTML = latinWords;
+  };
+
+displayLatin();
