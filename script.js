4446d8edc22b0b0e7a51618b2ed3ca4cb1450ee4
Brendan Magee <bmagee@gmail.com>
2016-02-24 11:32:16 -0500

    succeeded in cycling through cards

--- script.js
+++ script.js
@@ -3,20 +3,29 @@
 
 */
 var form = document.getElementById("submissionForm");
-var input = document.querySelector('#answer');
+var input = document.querySelector("#answer");
+var flashCard = document.querySelector(".flashCard");
+var i = 0;
 
 var vocabWords = [["m&emacr; (pronoun)" , "me"] , ["quid (pronoun)" , "what"] , ["nihil (noun)" , "nothing"] , ["n&omacr;n (adverb)" , "not"] , ["saepe (adverb)" , "often"] , ["s&imacr; (conjunction)" , "if"] , ["amo, amare, amavi, amatum (verb)" , "to love"] , ["cogito, cogitare, cogitavi, cogitatum (verb)" , "to think"] , ["debeo, deb&emacr;re, debui, debitum (verb)" , "to owe"] , ["do, dare, dedi, datum (irreg. verb)" , "to give"] , ["erro, errare, erravi, erratum (verb)" , "to wander"] , ["laudo, laudare, laudavi, laudatum (verb)" , "to praise"] , ["moneo, mon&emacr;re, monui, monitum (verb)" , "to remind"] , ["salveo, salv&emacr;re, &mdash; (verb)" , "to be well"] , ["servo, servare, servavi, servatum (verb)" , "to preserve"] , ["conservo, conservare, conservavi, conservatum (verb)" , "to conserve"] , ["terreo, terr&emacr;re, terrui, territum (verb)" , "to frighten"] , ["valeo, val&emacr;re, valui, valiturum (verb)" , "to be strong"] , ["video, vid&emacr;re, vidi, visum (verb)" , "to see"] , ["voco, vocare, vocavi, vocatus (verb)" , "to call"]];
 
-var displayWord = function() {
-    document.querySelector(".flashCard").innerHTML = vocabWords[0][0];
-};
+var displayWord = function(i) {
+  flashCard.innerHTML = vocabWords[i][0];
+  };
 
 form.addEventListener("submit" , function(evt){
   evt.preventDefault();
-  if (input.value === vocabWords[0][1]) {
+  if (input.value === vocabWords[i][1]) {
     alert("You got it!");
-  } else alert("Sorry, but that's incorrect.")
+    i++;
+    displayWord(i);
+    } else {
+    alert("Sorry, but that's incorrect.");
+    i++;
+    displayWord(i);
+  }
 });
+
 
 // var shuffleCards = function() {
 //   vocabWords.sort(function() {
@@ -31,4 +40,4 @@ form.addEventListener("submit" , function(evt){
 
 
 
-displayWord();
+displayWord(i);
