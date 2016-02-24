718a47f8f1fb89a77ad788e3bfc94c9b1728201c
Brendan Magee <bmagee@gmail.com>
2016-02-24 11:51:51 -0500

    added on-page notifications of accuracy

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 var form = document.getElementById("submissionForm");
 var input = document.querySelector("#answer");
 var flashCard = document.querySelector(".flashCard");
+var answerAccuracy = document.querySelector(".answerAccuracy")
 var i = 0;
 
 var vocabWords = [["m&emacr; (pronoun)" , "me"] , ["quid (pronoun)" , "what"] , ["nihil (noun)" , "nothing"] , ["n&omacr;n (adverb)" , "not"] , ["saepe (adverb)" , "often"] , ["s&imacr; (conjunction)" , "if"] , ["amo, amare, amavi, amatum (verb)" , "to love"] , ["cogito, cogitare, cogitavi, cogitatum (verb)" , "to think"] , ["debeo, deb&emacr;re, debui, debitum (verb)" , "to owe"] , ["do, dare, dedi, datum (irreg. verb)" , "to give"] , ["erro, errare, erravi, erratum (verb)" , "to wander"] , ["laudo, laudare, laudavi, laudatum (verb)" , "to praise"] , ["moneo, mon&emacr;re, monui, monitum (verb)" , "to remind"] , ["salveo, salv&emacr;re, &mdash; (verb)" , "to be well"] , ["servo, servare, servavi, servatum (verb)" , "to preserve"] , ["conservo, conservare, conservavi, conservatum (verb)" , "to conserve"] , ["terreo, terr&emacr;re, terrui, territum (verb)" , "to frighten"] , ["valeo, val&emacr;re, valui, valiturum (verb)" , "to be strong"] , ["video, vid&emacr;re, vidi, visum (verb)" , "to see"] , ["voco, vocare, vocavi, vocatus (verb)" , "to call"]];
@@ -15,12 +16,13 @@ var displayWord = function(i) {
 
 form.addEventListener("submit" , function(evt){
   evt.preventDefault();
+  flashCard.innerHTML = vocabWords[i][1];
   if (input.value === vocabWords[i][1]) {
-    alert("You got it!");
+    answerAccuracy.innerHTML = "You got it!";
     i++;
     displayWord(i);
     } else {
-    alert("Sorry, but that's incorrect.");
+    answerAccuracy.innerHTML = "Sorry, but that's incorrect.";
     i++;
     displayWord(i);
   }
